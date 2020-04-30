f03144cf808b5153152c607d61de4577f848df2e
Yifei Dong <yifdong@student.ethz.ch>
2020-04-30 19:11:42 +0200

    Update compute_controller_base_parameters.m

--- compute_controller_base_parameters.m
+++ compute_controller_base_parameters.m
@@ -19,6 +19,8 @@ function param = compute_controller_base_parameters
     Ts = 60;
     A = eye(3) + Ts .* [-(a1o+a12)/m1, a12/m1, 0; a12/m2, -(a12+a23+a2o)/m2, a23/m2; 0, a23/m3, -(a23+a3o)/m3 ]; 
     B = Ts .* [1/m1, 0; 0, 1/m2; 0, 0];
+    B_d = Ts .* [1/m1 0 0; 0 0 1/m2;0 0 1/m3];
+    C = eye(3);
     
     % (3) set point computation
     T_sp1 = b_ref(1);
@@ -44,7 +46,14 @@ function param = compute_controller_base_parameters
     
     % (6) Terminal cost computation via LQR
     [K, P] = dlqr(A, B, Q, R);
-        
+    
+    % (22) Offset free control
+    A_aug = [A B_d; zeros(3), eye(3)];
+    B_aug = [B; zeros(3,2)];
+    C_aug = [C, zeros(3)];
+    D_aug = zeros(3,2);
+    %eigen = eig(A_aug - L * C_aug);
+    
     % put everything together
     param.A = A;
     param.B = B;
@@ -57,4 +66,11 @@ function param = compute_controller_base_parameters
     param.Xcons = Xcons;
     param.Tcons = Tcons;
     param.Pcons = Pcons;
+    param.B_d = B_d;
+    param.C = C;
+    param.A_aug = A_aug;
+    param.B_aug = B_aug;
+    param.C_aug = C_aug;
+    param.D_aug = D_aug;
+    
 end
